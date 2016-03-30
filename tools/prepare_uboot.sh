2ac265bf168f964cdd4a8c07e0f1b8c175505bc1
Patrick Bruenn <p.bruenn@beckhoff.com>
2016-03-30 14:15:16 +0200

    u-boot: remove auto commit from prepare_uboot.sh
    - omit trouble on systems without .gitconfig and be more consistent with
      prepare_kernel.sh

--- tools/prepare_uboot.sh
+++ tools/prepare_uboot.sh
@@ -16,6 +16,3 @@ git checkout ${VERSION} -b dev-${VERSION}
 patch -p1 < ../u-boot-patches/0001_fix_iomux_for_uart2.patch
 patch -p1 < ../u-boot-patches/0002_set_ufcr_dte_for_cx9020_uart.patch
 patch -p1 < ../u-boot-patches/0003_add_beckhoff_cx9020.patch
-
-git add *
-git commit -m "apply cx9020 patches"
