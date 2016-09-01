c97c64641b2db3400ba15c89dfbcb3466015b670
Patrick Bruenn <p.bruenn@beckhoff.com>
2016-09-01 10:54:11 +0200

    cx9020: upgrade to u-boot v2016.07

--- tools/prepare_uboot.sh
+++ tools/prepare_uboot.sh
@@ -4,7 +4,7 @@ set -e
 set -o nounset
 
 if [ $# -ne 1 ]; then
-	echo -e "Usage:\n $0 <u-boot version>\n\nexample:\n $0 v2015.07"
+	echo -e "Usage:\n $0 <u-boot version>\n\nexample:\n $0 v2016.07"
 	exit 64
 fi
 
@@ -13,6 +13,5 @@ VERSION=$1
 git clone git://git.denx.de/u-boot.git
 pushd u-boot/
 git checkout ${VERSION} -b dev-${VERSION}
-patch -p1 < ../u-boot-patches/0001_fix_iomux_for_uart2.patch
-patch -p1 < ../u-boot-patches/0002_set_ufcr_dte_for_cx9020_uart.patch
-patch -p1 < ../u-boot-patches/0003_add_beckhoff_cx9020.patch
+git am -3 < ../u-boot-patches/0001-serial-mxc-add-support-for-UFCR_DTE.patch
+git am -3 < ../u-boot-patches/0002-board-mx53cx9020-add-support-for-Beckhoff-CX9020-010.patch
