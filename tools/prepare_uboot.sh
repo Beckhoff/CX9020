d7850508db9d8ebda06048e48102c3f6c09a8fac
Patrick Bruenn <p.bruenn@beckhoff.com>
2016-09-02 06:55:44 +0200

    cx9020: use GIT_CLONE_ARGS for uboot repo cloning

--- tools/prepare_uboot.sh
+++ tools/prepare_uboot.sh
@@ -10,7 +10,7 @@ fi
 
 VERSION=$1
 
-git clone git://git.denx.de/u-boot.git
+git clone git://git.denx.de/u-boot.git ${GIT_CLONE_ARGS}
 pushd u-boot/
 git checkout ${VERSION} -b dev-${VERSION}
 git am -3 < ../u-boot-patches/0001-serial-mxc-add-support-for-UFCR_DTE.patch
