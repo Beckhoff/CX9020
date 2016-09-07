bb0ea904708ab1109a53c06e021b7e7b0a9e484f
Patrick Bruenn <p.bruenn@beckhoff.com>
2016-09-07 09:08:36 +0200

    cx9020: upgrade to kernel v4.4.19-rt27
    - update imx53-cx9020.dts to be compatible with new devicetree parsing
      in gpu/drm/imx gpu/ipu-v3
    - change kernel config to include imx-ipuv3-crtc statically, since
      autoload of module seems broken, now

--- tools/prepare_kernel.sh
+++ tools/prepare_kernel.sh
@@ -6,7 +6,7 @@ set -e
 
 if [ "$#" -ne 3 ]; then
 
-        echo -e "Usage:\n $0 <VERSION> <PATCH> <RT-PATCH>\n\nexample: to checkout kernel 4.1.31 with rt35\n $0 4.1 31 35"
+        echo -e "Usage:\n $0 <VERSION> <PATCH> <RT-PATCH>\n\nexample: to checkout kernel 4.4.19 with rt27\n $0 4.4 19 27"
         exit 64
 fi
 
@@ -15,17 +15,20 @@ PATCH=$2
 FULL_VERSION=${VERSION}.${PATCH}
 RT_VERSION=${FULL_VERSION}-rt${3}
 REPO=kernel
+GIT_REMOTE=git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git
+#GIT_REMOTE=git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git
 
 # clone a clean linux-rt-devel repository
-git clone git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git ${REPO} ${GIT_CLONE_ARGS}
+git clone ${GIT_REMOTE} ${REPO} ${GIT_CLONE_ARGS}
 pushd ${REPO}
-git checkout -b dev-${RT_VERSION} v${RT_VERSION}
+git checkout v${RT_VERSION} -b dev-${RT_VERSION}
 
 # apply cx9020 patches
-git am < ../kernel-patches/0001-clk-imx5-ipu_di_sel-clocks-can-set-parent-rates.patch
-git am < ../kernel-patches/0002-imx-parallel-display-add-ddc-support.patch
-git am < ../kernel-patches/0003-ARM-dts-imx-add-CX9020-Embedded-PC-device-tree.patch
-git am < ../kernel-patches/0005-ARM-dts-imx-add-rtc-for-CX9020.patch
+git am ../kernel-patches/0001-clk-imx5-ipu_di_sel-clocks-can-set-parent-rates.patch
+git am ../kernel-patches/0002-imx53.dtsi-Add-IPU-nodes-for-csi.patch
+git am ../kernel-patches/0003-drm-panel-simple-Add-support-for-ddc-only-panel.patch
+git am ../kernel-patches/0004-ARM-dts-imx-add-CX9020-Embedded-PC-device-tree.patch
+git am ../kernel-patches/0005-net-phy-multiplex-switch-phy-ports.patch
 
 # apply prepared config
 cp -a ../kernel-patches/config-CX9020 .config
