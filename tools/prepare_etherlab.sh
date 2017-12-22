b02c9d4f89cc7900203c52e5c95e491da3cd65e6
Patrick Bruenn <p.bruenn@beckhoff.com>
2017-12-22 09:54:40 +0100

    etherlab: use single patches instead of bundle

--- tools/prepare_etherlab.sh
+++ tools/prepare_etherlab.sh
@@ -5,7 +5,5 @@ hg clone http://hg.code.sf.net/p/etherlabmaster/code ethercat-hg
 
 pushd ethercat-hg/
 hg update stable-1.5
-cp ../ccat/etherlab.bundle .
-hg unbundle etherlab.bundle
-hg update
+hg import ../ccat/etherlab-patches/000*
 ./bootstrap
