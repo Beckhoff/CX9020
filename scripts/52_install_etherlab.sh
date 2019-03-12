348549170ae39c4c118f97815471545d320c0063
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2019-03-12 11:38:28 +0100

    etherlab: update for systemd changes and module loading
    upstream etherlab changed systemd pathes

--- scripts/52_install_etherlab.sh
+++ scripts/52_install_etherlab.sh
@@ -25,8 +25,10 @@ make ARCH=arm CROSS_COMPILE=${CROSS_PREFIX} DESTDIR=${ROOTFS_MOUNT} install
 popd
 
 mkdir -p ${ROOTFS_MOUNT}/etc/sysconfig/
-cp -a ${ROOTFS_MOUNT}/opt/etherlab/etc/sysconfig/ethercat ${ROOTFS_MOUNT}/etc/sysconfig/
-sed -b -i 's/MASTER0_DEVICE=\"\"/MASTER0_DEVICE=\"ff:ff:ff:ff:ff:ff\"/' ${ROOTFS_MOUNT}/etc/sysconfig/ethercat
-sed -b -i 's/DEVICE_MODULES=\"\"/DEVICE_MODULES=\"ccat_netdev\"/' ${ROOTFS_MOUNT}/etc/sysconfig/ethercat
-ln -fs /opt/etherlab/etc/init.d/ethercat ${ROOTFS_MOUNT}/etc/init.d/
+cp -a ${ROOTFS_MOUNT}/usr/local/etc/sysconfig/ethercat ${ROOTFS_MOUNT}/etc/ethercat.conf
+sed -b -i 's/MASTER0_DEVICE=\"\"/MASTER0_DEVICE=\"ff:ff:ff:ff:ff:ff\"/' ${ROOTFS_MOUNT}/etc/ethercat.conf
+sed -b -i 's/DEVICE_MODULES=\"\"/DEVICE_MODULES=\"ccat_netdev\"/' ${ROOTFS_MOUNT}/etc/ethercat.conf
+ln -fs /usr/local/etc/init.d/ethercat ${ROOTFS_MOUNT}/etc/init.d/
+printf "ccat\n" > ${ROOTFS_MOUNT}/etc/modules-load.d/ccat.conf
+sudo chroot ${ROOTFS_MOUNT} /bin/bash -c "systemctl enable ethercat"
 fi
