da81ec34707d9feb6500cb183cdefffa8b7311dc
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2019-04-04 09:04:26 +0200

    run_build: use apt cache if available
    We use the APT_CACHE_SERVER variable as http proxy if it's available. If
    not no proxy will be used.

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 set -e
-set -o nounset
 
 tools/prepare_uboot.sh v2018.11
 make uboot-tests
@@ -10,4 +8,4 @@ tools/prepare_kernel.sh v4.20
 make kernel
 tools/prepare_etherlab.sh
 make etherlab
-sudo scripts/build_sdimage.sh
+sudo http_proxy=${APT_CACHE_SERVER} scripts/build_sdimage.sh
