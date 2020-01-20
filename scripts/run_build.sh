c7b7738359da7ca06fe62eb14a73b39b263aadea
Patrick Bruenn <p.bruenn@beckhoff.com>
2020-01-20 16:26:14 +0100

    ci: add support for multiple EtherCAT master variants

--- scripts/run_build.sh
+++ scripts/run_build.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2019 - 2020 Beckhoff Automation GmbH & Co. KG
+
 set -e
+set -u
+
+readonly variant="${CI_JOB_NAME##*:}"
 
 tools/prepare_uboot.sh v2019.10
 make uboot
 tools/prepare_kernel.sh v4.19-rt
 make kernel
-tools/prepare_etherlab.sh
-make etherlab
+tools/prepare_${variant}.sh
+make ${variant}
 sudo http_proxy=${APT_CACHE_SERVER} scripts/build_sdimage.sh
